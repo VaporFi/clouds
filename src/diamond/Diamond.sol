83fc29cefc520b8e55b459b61828d9d9a8f8bfa5
mektigboy <mektigboy@protonmail.com>
2022-09-26 08:45:12 -0500

    Update Diamond.sol

---
+++ src/diamond/Diamond.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IDiamondCut} from "../interfaces/diamond/IDiamondCut.sol";
+
+error Diamond__InitializationFailed(
+    address initializationContractAddress,
+    bytes data
+);
+error Diamond__OnlyOwner();
+
+contract Diamond {
+    //////////////
+    /// EVENTS ///
+    //////////////
+
+    event DiamondCut(
+        IDiamondCut.FacetCut[] diamondCut,
+        address initializable,
+        bytes data
+    );
+
+    event OwnershipTransferred(
+        address indexed oldOwner,
+        address indexed newOwner
+    );
+
+    ///////////////
+    /// STORAGE ///
+    ///////////////
+
+    bytes32 constant DIAMOND_STORAGE_POSITION =
+        keccak256("diamond.standard.diamond.storage");
+
+    /////////////////
+    /// STRUCTURE ///
+    /////////////////
+
+    struct FacetAddressAndPosition {
+        address facetAddress;
+        uint96 functionSelectorPosition;
+    }
+
+    struct FacetFunctionSelectors {
+        bytes4[] functionSelectors;
+        uint256 facetAddressPosition; // Position of 'facetAddress' in 'facetAddresses' array
+    }
+
+    struct DiamondStorage {
+        mapping(bytes4 => FacetAddressAndPosition) selectorToFacetAndPosition;
+        mapping(address => FacetFunctionSelectors) facetFunctionSelectors;
+        address[] facetAddresses;
+        mapping(bytes4 => bool) supportedInterfaces;
+        address contractOwner;
+    }
+
+    function diamondStorage()
+        internal
+        pure
+        returns (DiamondStorage storage returnedDiamondStorage)
+    {
+        bytes32 position = DIAMOND_STORAGE_POSITION;
+
+        assembly {
+            returnedDiamondStorage.slot := position
+        }
+    }
+
+    function setContractOwner(address _newOwner) internal {
+        DiamondStorage storage ds = diamondStorage();
+
+        address oldOwner = ds.contractOwner;
+
+        ds.contractOwner = _newOwner;
+
+        emit OwnershipTransferred(oldOwner, _newOwner);
+    }
+
+    function contractOwner()
+        internal
+        view
+        returns (address returnedContractOwner)
+    {
+        returnedContractOwner = diamondStorage().contractOwner;
+    }
+
+    function enforceIsContractOwner() internal view {
+        if (msg.sender != diamondStorage().contractOwner)
+            revert Diamond__OnlyOwner();
+    }
+
+    function diamondCut(
+        IDiamondCut.FacetCut[] memory _diamondCut,
+        address _initializable,
+        bytes memory _data
+    ) internal {}
+}
