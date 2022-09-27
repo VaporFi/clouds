5e40ec006443bf2e6e7d138dfd06a14278a84195
mektigboy <mektigboy@protonmail.com>
2022-09-27 09:15:49 -0500

    Update diamond/DiamondInit.sol

--- src/diamond/DiamondInit.sol
+++ src/diamond/DiamondInit.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Diamond} from "./Diamond.sol";
 import {IDiamondCut} from "../interfaces/diamond/IDiamondCut.sol";
 import {IDiamondLoupe} from "../interfaces/diamond/IDiamondLoupe.sol";
 import {IERC165} from "../interfaces/IERC165.sol";
 import {IERC173} from "../interfaces/IERC173.sol";
+import {LibDiamond} from "./LibDiamond.sol";
 
 contract DiamondInit {
     function init() external {
-        Diamond.DiamondStorage storage ds = Diamond.diamondStorage();
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
 
         ds.supportedInterfaces[type(IERC165).interfaceId] = true;
         ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
