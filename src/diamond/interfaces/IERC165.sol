7084a3cf5c38aaa34943509264c789449103fb9b
mektigboy <mektigboy@protonmail.com>
2022-11-04 17:37:02 +0800

    Update IERC165.sol

--- src/diamond/interfaces/IERC165.sol
+++ src/diamond/interfaces/IERC165.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
 /// @title IERC165
 /// @author mektigboy
-/// @author Modified from Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
-/// @dev EIP-165 standard
 interface IERC165 {
     /////////////
     /// LOGIC ///
     /////////////
 
-    /// @notice ...
-    /// @param _id Interface ID
-    function supportsInterface(bytes4 _id) external view returns (bool);
+    function supportsInterface(bytes4 id) external view returns (bool);
 }
