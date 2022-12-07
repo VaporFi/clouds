b45cf1157a4168299bf8d74435d81a1b84ded86e
mektigboy <mektigboy@protonmail.com>
2022-12-07 11:18:52 +0800

    Update Ownable.sol

---
+++ src/auth/Ownable.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+error Ownable__OnlyOwner();
+
+/// @title Ownable
+/// @author mektigboy
+/// @author Modified from Solmate: https://github.com/transmissions11/solmate
+/// @notice Simple single owner authorization mixin
+abstract contract Ownable {
+    //////////////
+    /// EVENTS ///
+    //////////////
+
+    event OwnershipTransferred(address indexed user, address indexed newOwner);
+
+    ///////////////
+    /// STORAGE ///
+    ///////////////
+
+    address public owner;
+
+    /////////////////
+    /// MODIFIERS ///
+    /////////////////
+
+    modifier onlyOwner() virtual {
+        if (msg.sender != owner) revert Ownable__OnlyOwner();
+        _;
+    }
+
+    /////////////
+    /// LOGIC ///
+    /////////////
+
+    constructor(address _owner) {
+        owner = _owner;
+
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        owner = newOwner;
+
+        emit OwnershipTransferred(msg.sender, newOwner);
+    }
+}
